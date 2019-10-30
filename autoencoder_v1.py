3d9ed966b9db89c7e64346b34ad737daa779321a
Sachin Umrao <troublemaker1512@gmail.com>
2019-10-31 02:49:26 +0530

    bug fix for gpu usage

--- autoencoder_v1.py
+++ autoencoder_v1.py
@@ -1,34 +1,41 @@
 # import dependencies
 from time import time
-
+from tqdm import tqdm
 import numpy as np
 import torch
 import torchvision
 from torch import nn, optim
 from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
+from torch.backends import cudnn
+
+# cuda optimizations
+#cudnn.benchmark = True
+
 
 # normalize data
 transform = transforms.Compose([transforms.ToTensor(),
-                               transforms.Normalize((0.5,),(0.5))
+                               transforms.Normalize((0.5,),(0.5,))
                                ])
 
 # load data
+print("Downloading data...\n")
+
 trainset = datasets.MNIST('./data', download=True, train=True, 
                           transform=transform)
 
 valset = datasets.MNIST('./data', download=True, train=False, 
                           transform=transform)
 
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=64,
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=128,
                                           shuffle=True)
 
-valloader = torch.utils.data.DataLoader(valset, batch_size=64,
+valloader = torch.utils.data.DataLoader(valset, batch_size=128,
                                           shuffle=True)
 
 # model params
 input_size = 784
-hidden_size = [512, 256, 128, 256, 512]
+hidden_size = [512, 256, 128, 300, 600]
 op_size = 784
 
 # model architecture
@@ -44,6 +51,10 @@ model = nn.Sequential(nn.Linear(input_size, hidden_size[0]),
                       nn.ReLU(),
                       nn.Linear(hidden_size[4], op_size))
 
+# copy the model in gpu
+model.cuda()
+
+print("Model Summary \n")
 print(model)
 
 # use MSE loss 
@@ -53,31 +64,41 @@ optimizer = optim.Adam(model.parameters(), lr=0.001)
 time_0 = time()
 n_epochs = 20
 
+print("Starting training loop...")
 # train loop
+loss_vals = []
 for e in range(n_epochs):
-    running_loss = 0
-    for images, labels in trainloader:
-        images = images.view(images.shape[0], -1)
+    
+    loss = None
+    for images, labels in tqdm(trainloader, desc="Epoch: "+str(e+1)+"/20 :"):
+        images = images.view(images.shape[0], -1).cuda()
         optimizer.zero_grad()
         outputs = model(images)
         loss = criterion(outputs, images)
         loss.backward()
         optimizer.step()
-        running_loss += loss.item()
+    print("Loss : ", loss.cpu().item())
+    loss_vals.append(loss.cpu().item())
 
-
+print("Finished Training...")
 print("\nTraining Time (mins) :",(time() - time_0)/60)
 
+print("Plotting Training Curve...")
+plt.plot(loss_vals)
+plt.show()
+
+# score images from validation set
 images, labels = next(iter(valloader))
 img = images[0].view(1,784)
 with torch.no_grad():
+    img = img.cuda()
     ops = model(img)
 
-img_p = ops.view(28,28).numpy()
+img_p = ops.cpu().view(28,28).numpy()
 
 # plot original and oputput
 plt.imshow(img_p, cmap='gray')
 plt.show()
 
-plt.imshow(img.view(28,28).numpy(), cmap='gray')
+plt.imshow(img.cpu().view(28,28).numpy(), cmap='gray')
 plt.show()
