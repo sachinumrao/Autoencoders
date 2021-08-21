408913a03725928125ce0c7d3d4f09215f44b459
sachinumrao <sachin.umrao.1512@gmail.com>
2021-08-21 21:18:54 +0530

    Added train and test loader for cifar10

--- Torch_AE/cifar_autoencoder.py
+++ Torch_AE/cifar_autoencoder.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-
+from torchsummary import summary
 
 class AutoEncoder(nn.Module):
     """
@@ -10,23 +10,55 @@ class AutoEncoder(nn.Module):
         super(AutoEncoder, self).__init__()
         
         # define encoder blocks
-        self.conv1 = nn.Conv2d(3, 16, 3, stride=1)
+        self.conv1 = nn.Conv2d(3, 16, 3, stride=1, padding="same")
         self.relu1 = nn.ReLU()
         self.maxpool1 = nn.MaxPool2d(2, stride=2)
         
-        self.conv2 = nn.Conv2d(16, 32, 3, stride=1)
+        self.conv2 = nn.Conv2d(16, 32, 3, stride=1, padding="same")
         self.relu2 = nn.ReLU()
         self.maxpool2 = nn.MaxPool2d(2, stride=2)
         
-        self.conv3 = nn.Conv2d(32, 64, 3, stride=1)
+        self.conv3 = nn.Conv2d(32, 64, 3, stride=1, padding="same")
         self.relu3 = nn.ReLU()
         self.maxpool3 = nn.MaxPool2d(2, stride=2)
         
         # define decoder blocks
-        pass
+        self.t_conv1 = nn.ConvTranspose2d(64, 32, 2, stride=2)
+        self.t_relu1 = nn.ReLU()
+        self.t_conv2 = nn.ConvTranspose2d(32, 8, 2, stride=2)
+        self.t_relu2 = nn.ReLU()
+        self.t_conv3 = nn.ConvTranspose2d(8, 3, 2, stride=2)
+        self.t_sigmoid = nn.Sigmoid()
     
     def forward(self, x):
-        pass
-
+        # encoder block
+        out = self.conv1(x)
+        out = self.relu1(out)
+        out = self.maxpool1(out)
+        
+        out = self.conv2(out)
+        out = self.relu2(out)
+        out = self.maxpool2(out)
+        
+        out = self.conv3(out)
+        out = self.relu3(out)
+        out = self.maxpool3(out)
+        
+        # decoder block
+        out = self.t_conv1(out)
+        out = self.t_relu1(out)
+        
+        out = self.t_conv2(out)
+        out = self.t_relu2(out)
+        
+        out = self.t_conv3(out)
+        out = self.t_sigmoid(out)
+        
+        return out
     
+    
+if __name__ == '__main__':
+    model = AutoEncoder()
+    # print(model)
+    print(summary(model, input_size=(3, 32, 32)))
     
--- Torch_AE/train.py
+++ Torch_AE/train.py
@@ -1,8 +1,11 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
 
 import numpy as np
+from torchvision.transforms.transforms import Normalize, ToTensor
 import wandb as wb
 import matplotlib as mpl
 
@@ -11,8 +14,48 @@ from cifar_autoencoder import AutoEncoder
 
 # get dataloaders
 def get_cifar_dataloaders():
-    pass
+    """
+    Get train and test dataloaders for CIFAR-10 dataset.
+    """
+    transform = transforms.Compose(
+        [ToTensor(),
+         Normalize((0.4914, 0.4822, 0.4466), (0.247, 0.243, 0.261)),])
+    
+    trainset = datasets.CIFAR10(root='~/Data/torchvision/', 
+                                train=True, 
+                                download=True, 
+                                transform=transform)
+    
+    testset = datasets.CIFAR10(root='~/Data/torchvision/', 
+                               train=True, 
+                               download=True, 
+                               transform=transform)
+    
+    train_dataloader = DataLoader(trainset,
+                                  batch_size=config.BS,
+                                  shuffle=False,
+                                  num_workers=2)
+    
+    test_dataloader = DataLoader(testset,
+                                 batch_size=config.BS,
+                                 shuffle=False,
+                                 num_workers=2)
+    
+    return train_dataloader, test_dataloader
 
+
+def get_device():
+    """
+    Get device to train on.
+    """
+    if torch.cuda.is_available():
+        device = 'cuda'
+    else:
+        device = 'cpu'
+        
+    return device
+
+    
 # train for one epoch
 def train_one_epoch():
     pass
