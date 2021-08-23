d565fe33ca6cb00c58374a04b15d7bcfdd51b8e1
sachinumrao <sachin.umrao.1512@gmail.com>
2021-08-24 03:08:00 +0530

    Added train function. integrated wandb logging

--- Torch_AE/train.py
+++ Torch_AE/train.py
@@ -6,12 +6,15 @@ from torchvision import datasets, transforms
 
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
+import wandb
 from torchvision.transforms.transforms import Normalize, ToTensor
 import wandb as wb
-import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 import config
 from cifar_autoencoder import AutoEncoder
+
 
 # get dataloaders
 def get_cifar_dataloaders():
@@ -28,19 +31,19 @@ def get_cifar_dataloaders():
                                 transform=transform)
     
     testset = datasets.CIFAR10(root='~/Data/torchvision/', 
-                               train=True, 
+                               train=False, 
                                download=True, 
                                transform=transform)
     
     train_dataloader = DataLoader(trainset,
                                   batch_size=config.BS,
                                   shuffle=False,
-                                  num_workers=2)
+                                  num_workers=0)
     
     test_dataloader = DataLoader(testset,
                                  batch_size=config.BS,
                                  shuffle=False,
-                                 num_workers=2)
+                                 num_workers=0)
     
     return train_dataloader, test_dataloader
 
@@ -59,15 +62,60 @@ def get_device():
     
 # train for one epoch
 def train_one_epoch(model, train_dataloader, loss, optimizer, device):
+    model.to(device)
+    model.train()
     
-    pass
+    train_loss = 0.0
+    for data in tqdm(train_dataloader):
+        # get data
+        inputs, _ = data
+        inputs = inputs.to(device)
+        
+        # forward
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss_ = loss(outputs, inputs)
+        loss_.backward()
+        optimizer.step()
+        
+        # calculate loss
+        train_loss += loss_.item()*inputs.size(0)
+        wandb.log({"batch_loss": loss_.item()})
+    
+    train_loss = train_loss / len(train_dataloader)
+    wandb.log({"train_loss": train_loss})
+    
+    return train_loss, model
+
 
 # run validation after model epoch
-def validate(model, dataloader):
-    pass
+def validate(model, dataloader, loss, device):
+    model.to(device)
+    model.eval()
+    test_loss = 0.0
+    for data in tqdm(dataloader):
+        # get data
+        inputs, _ = data
+        inputs = inputs.to(device)
+        
+        # forward
+        outputs = model(inputs)
+        loss_ = loss(outputs, inputs)
+        
+        # calculate loss
+        test_loss += loss_.item()*inputs.size(0)
+        
+    test_loss = test_loss / len(dataloader)
+    wandb.log({"test_loss": test_loss})
+    
+    return test_loss
+
 
 # main train loop
 def train(model, train_dataloader, test_dataloader, device):
+    # set up tracking
+    wandb.init(project='cifar10_autoencoder', entity='sumrao')
+    wandb.watch(model)
     train_loss = []
     test_loss = []
     # define loss function
@@ -88,8 +136,8 @@ def train(model, train_dataloader, test_dataloader, device):
         test_loss.append(test_loss_epoch)
         
         # print loss information
-        print(f'Epoch: {i+1}/{config.EPOCHS} \t Train Loss: {train_loss_epoch}')
-        print(f'Epoch: {i+1}/{config.EPOCHS} \t Test Loss: {test_loss_epoch}')
+        print(f'Epoch: {i+1}/{config.EPOCHS} \t Train Loss: {train_loss_epoch:.6f}')
+        print(f'Epoch: {i+1}/{config.EPOCHS} \t Test Loss: {test_loss_epoch:.6f}')
         
         # save model
         if (i+1)%5 == 0:
@@ -98,11 +146,12 @@ def train(model, train_dataloader, test_dataloader, device):
     loss_dict = {'train_loss': train_loss, 'test_loss': test_loss}
     return loss_dict, model
 
+
 # save model checkpoint
 def save_model(model, epoch):
     output_dir = './model_checkpoint/'
     model_name = "autoencoder" + "_" + str(epoch) + ".pth"
-    model.save(output_dir + model_name)
+    torch.save(model, output_dir + model_name)
     
 
 # load model
@@ -110,20 +159,57 @@ def load_model():
     model = torch.load('./model_checkpoint/autoencoder_100.pth')
     return model
 
+
 # run inference from model
 def run_inference(test_dataloader, device):
+    classes = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck']
+    
     model = load_model()
+    model.to(device)
+    
+    dataiter = iter(test_dataloader)
+    images, labels = dataiter.next()
+    images = images.to(device)
+    
+    output = model(images)
+    
+    output = output.cpu().detach().numpy()
+    images = images.cpu().detach().numpy()
+    
+    
+    
+    # plot original images
+    fig, axes = plt.subplots(nrows=2, ncols=5, sharex=True, sharey=True,
+                                 figsize=(12,10))
+    for idx in range(5):
+        ax = fig.add_subplot(2,5, idx+1, xticks=[], yticks=[])
+        img = images[idx,:,:,:].squeeze()
+        img = np.transpose(img, (1, 2, 0))
+        # img = (img * 255).astype(np.uint8)
+        plt.imshow(img)
+        ax.set_title(classes[labels[idx]])
+        
+        ax = fig.add_subplot(2,5, idx+5+1, xticks=[], yticks=[])
+        rst = output[idx,:,:,:].squeeze()
+        rst = np.transpose(rst, (1, 2, 0))
+        # rst = (rst * 255).astype(np.uint8)
+        plt.imshow(rst)
+        
+        ax.set_title(classes[labels[idx]])
+    plt.savefig("Results.png")
+  
     
 # save traning loss
 def save_loss(loss_dict):
     ldf = pd.DataFrame.from_dict(loss_dict)
-    mpl.plt.plot(ldf['train_loss'], label='train_loss')
-    mpl.plt.plot(ldf['test_loss'], label='test_loss')
-    mpl.plt.legend()
-    mpl.plt.xlabel('Epoch')
-    mpl.plt.ylabel('Loss')
-    mpl.plt.title('Loss vs Epoch')
-    mpl.plt.savefig('loss.png')
+    plt.plot(ldf['train_loss'], label='train_loss')
+    plt.plot(ldf['test_loss'], label='test_loss')
+    plt.legend()
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.title('Loss vs Epoch')
+    plt.savefig('loss.png')
     
 
 # main function
@@ -138,7 +224,10 @@ def main():
     device = get_device()
     
     # train model
-    loss_dict, model = train(model, train_dataloader, test_dataloader, device)
+    loss_dict, model = train(model, 
+                             train_dataloader, 
+                             test_dataloader, 
+                             device)
     
     # save model checkpoint
     save_model(model, 100)
