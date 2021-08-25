9144cde9b84ac0d20eadd3c1aa96c836ea093394
sachinumrao <sachin.umrao.1512@gmail.com>
2021-08-25 22:53:40 +0530

    template code for conv vae

---
+++ Torch_VAE/cifar_vae.py
@@ -0,0 +1,73 @@
+import torch
+import torch.nn as nn
+from torchsummary import summary
+
+class VariationalAutoEncoder(nn.Module):
+    """
+    Convolutional Autoencoder for cifar10 dataset
+    """
+    def __init__(self, latent_dim, batch_size):
+        super(VariationalAutoEncoder, self).__init__()
+        
+        self.latent_dim = latent_dim
+        self.batch_size = batch_size
+        
+        # define encoder blocks
+        self.conv1 = nn.Conv2d(3, 16, 3, stride=1, padding=1)
+        self.relu1 = nn.ReLU()
+        self.maxpool1 = nn.MaxPool2d(2, stride=2)
+        
+        self.conv2 = nn.Conv2d(16, 32, 3, stride=1, padding=1)
+        self.relu2 = nn.ReLU()
+        self.maxpool2 = nn.MaxPool2d(2, stride=2)
+        
+        self.conv3 = nn.Conv2d(32, 64, 3, stride=1, padding=1)
+        self.relu3 = nn.ReLU()
+        self.maxpool3 = nn.MaxPool2d(2, stride=2)
+        
+        self.fc1 = nn.Linear(64*4*4, 128)
+        
+        self.fc_mu = nn.Linear(128, 64)
+        self.fc_logvar = nn.Linear(128, 64)
+        
+        
+        # define decoder blocks
+        self.t_conv1 = nn.ConvTranspose2d(64, 32, 2, stride=2)
+        self.t_relu1 = nn.ReLU()
+        self.t_conv2 = nn.ConvTranspose2d(32, 8, 2, stride=2)
+        self.t_relu2 = nn.ReLU()
+        self.t_conv3 = nn.ConvTranspose2d(8, 3, 2, stride=2)
+        self.t_sigmoid = nn.Sigmoid()
+    
+    def forward(self, x):
+        # encoder block
+        out = self.conv1(x)
+        out = self.relu1(out)
+        out = self.maxpool1(out)
+        
+        out = self.conv2(out)
+        out = self.relu2(out)
+        out = self.maxpool2(out)
+        
+        out = self.conv3(out)
+        out = self.relu3(out)
+        out = self.maxpool3(out)
+        
+        # decoder block
+        out = self.t_conv1(out)
+        out = self.t_relu1(out)
+        
+        out = self.t_conv2(out)
+        out = self.t_relu2(out)
+        
+        out = self.t_conv3(out)
+        out = self.t_sigmoid(out)
+        
+        return out
+    
+    
+if __name__ == '__main__':
+    model = VariationalAutoEncoder()
+    # print(model)
+    print(summary(model, input_size=(3, 32, 32)))
+    
---
+++ Torch_VAE/train.py
@@ -0,0 +1,242 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+# import wandb
+from torchvision.transforms.transforms import Normalize, ToTensor
+import matplotlib.pyplot as plt
+
+import config
+from cifar_vae import VariationalAutoEncoder
+
+
+# get dataloaders
+def get_cifar_dataloaders():
+    """
+    Get train and test dataloaders for CIFAR-10 dataset.
+    """
+    transform = transforms.Compose(
+        [ToTensor(),
+         Normalize((0.4914, 0.4822, 0.4466), (0.247, 0.243, 0.261)),])
+    
+    trainset = datasets.CIFAR10(root='~/Data/torchvision/', 
+                                train=True, 
+                                download=True, 
+                                transform=transform)
+    
+    testset = datasets.CIFAR10(root='~/Data/torchvision/', 
+                               train=False, 
+                               download=True, 
+                               transform=transform)
+    
+    train_dataloader = DataLoader(trainset,
+                                  batch_size=config.BS,
+                                  shuffle=False,
+                                  num_workers=0)
+    
+    test_dataloader = DataLoader(testset,
+                                 batch_size=config.BS,
+                                 shuffle=False,
+                                 num_workers=0)
+    
+    return train_dataloader, test_dataloader
+
+
+def get_device():
+    """
+    Get device to train on.
+    """
+    if torch.cuda.is_available():
+        device = 'cuda'
+    else:
+        device = 'cpu'
+        
+    return device
+
+    
+# train for one epoch
+def train_one_epoch(model, train_dataloader, loss, optimizer, device):
+    model.to(device)
+    model.train()
+    
+    train_loss = 0.0
+    for data in tqdm(train_dataloader):
+        # get data
+        inputs, _ = data
+        inputs = inputs.to(device)
+        
+        # forward
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss_ = loss(outputs, inputs)
+        loss_.backward()
+        optimizer.step()
+        
+        # calculate loss
+        train_loss += loss_.item()*inputs.size(0)
+        # wandb.log({"batch_loss": loss_.item()})
+    
+    train_loss = train_loss / len(train_dataloader)
+    # wandb.log({"train_loss": train_loss})
+    
+    return train_loss, model
+
+
+# run validation after model epoch
+def validate(model, dataloader, loss, device):
+    model.to(device)
+    model.eval()
+    test_loss = 0.0
+    for data in tqdm(dataloader):
+        # get data
+        inputs, _ = data
+        inputs = inputs.to(device)
+        
+        # forward
+        outputs = model(inputs)
+        loss_ = loss(outputs, inputs)
+        
+        # calculate loss
+        test_loss += loss_.item()*inputs.size(0)
+        
+    test_loss = test_loss / len(dataloader)
+    # wandb.log({"test_loss": test_loss})
+    
+    return test_loss
+
+
+# main train loop
+def train(model, train_dataloader, test_dataloader, device):
+    # set up tracking
+    # wandb.init(project='cifar10_vae', entity='sumrao')
+    # wandb.watch(model)
+    train_loss = []
+    test_loss = []
+    # define loss function
+    loss = nn.MSELoss()
+    # define optimizer
+    optimizer = optim.Adam(model.parameters(), lr=config.LR)
+    
+    for i in range(config.EPOCHS):
+        # train model for a epoch
+        train_loss_epoch, model = train_one_epoch(
+            model, train_dataloader, loss, optimizer, device)
+        
+        # validate model
+        test_loss_epoch = validate(model, test_dataloader, loss, device)
+        
+        # save loss
+        train_loss.append(train_loss_epoch)
+        test_loss.append(test_loss_epoch)
+        
+        # print loss information
+        print(f'Epoch: {i+1}/{config.EPOCHS} \t Train Loss: {train_loss_epoch:.6f}')
+        print(f'Epoch: {i+1}/{config.EPOCHS} \t Test Loss: {test_loss_epoch:.6f}')
+        
+        # save model
+        if (i+1)%5 == 0:
+            save_model(model, i)        
+    
+    loss_dict = {'train_loss': train_loss, 'test_loss': test_loss}
+    return loss_dict, model
+
+
+# save model checkpoint
+def save_model(model, epoch):
+    output_dir = './model_checkpoint/'
+    model_name = "autoencoder" + "_" + str(epoch) + ".pth"
+    torch.save(model, output_dir + model_name)
+    
+
+# load model
+def load_model():
+    model = torch.load('./model_checkpoint/autoencoder_100.pth')
+    return model
+
+
+# run inference from model
+def run_inference(test_dataloader, device):
+    classes = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck']
+    
+    model = load_model()
+    model.to(device)
+    
+    dataiter = iter(test_dataloader)
+    images, labels = dataiter.next()
+    images = images.to(device)
+    
+    output = model(images)
+    
+    output = output.cpu().detach().numpy()
+    images = images.cpu().detach().numpy()
+    
+    
+    
+    # plot original images
+    fig, axes = plt.subplots(nrows=2, ncols=5, sharex=True, sharey=True,
+                                 figsize=(12,10))
+    for idx in range(5):
+        ax = fig.add_subplot(2,5, idx+1, xticks=[], yticks=[])
+        img = images[idx,:,:,:].squeeze()
+        img = np.transpose(img, (1, 2, 0))
+        # img = (img * 255).astype(np.uint8)
+        plt.imshow(img)
+        ax.set_title(classes[labels[idx]])
+        
+        ax = fig.add_subplot(2,5, idx+5+1, xticks=[], yticks=[])
+        rst = output[idx,:,:,:].squeeze()
+        rst = np.transpose(rst, (1, 2, 0))
+        # rst = (rst * 255).astype(np.uint8)
+        plt.imshow(rst)
+        
+        ax.set_title(classes[labels[idx]])
+    plt.savefig("Results.png")
+  
+    
+# save traning loss
+def save_loss(loss_dict):
+    ldf = pd.DataFrame.from_dict(loss_dict)
+    plt.plot(ldf['train_loss'], label='train_loss')
+    plt.plot(ldf['test_loss'], label='test_loss')
+    plt.legend()
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.title('Loss vs Epoch')
+    plt.savefig('loss.png')
+    
+
+# main function
+def main():
+    # get dataloaders
+    train_dataloader, test_dataloader = get_cifar_dataloaders()
+    
+    # get models
+    model = VariationalAutoEncoder()
+    
+    # get device
+    device = get_device()
+    
+    # train model
+    loss_dict, model = train(model, 
+                             train_dataloader, 
+                             test_dataloader, 
+                             device)
+    
+    # save model checkpoint
+    save_model(model, 100)
+    
+    # save training loss
+    save_loss(loss_dict)
+    
+    # generate some images
+    run_inference(test_dataloader, device)
+    
+
+if __name__ == "__main__":
+    main()
