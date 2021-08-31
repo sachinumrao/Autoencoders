b891b095a28b100ead7df1f5cb8614d753005390
sachinumrao <sachin.umrao.1512@gmail.com>
2021-08-31 23:14:42 +0530

    refactored encoder and decoder blocks

--- Torch_AE/cifar_autoencoder.py
+++ Torch_AE/cifar_autoencoder.py
@@ -2,58 +2,69 @@ import torch
 import torch.nn as nn
 from torchsummary import summary
 
+# custom layer for reshape
+class Reshape(nn.Module):
+    """
+    Class to reshape the tensor
+    """
+    def __init__(self, *args):
+        super(Reshape, self).__init__()
+        self.shape = args
+
+    def forward(self, x):
+        return x.view(self.shape)
+    
+
 class AutoEncoder(nn.Module):
     """
     Convolutional Autoencoder for cifar10 dataset
     """
-    def __init__(self):
+    def __init__(self, latent_dim=32):
         super(AutoEncoder, self).__init__()
         
-        # define encoder blocks
-        self.conv1 = nn.Conv2d(3, 16, 3, stride=1, padding="same")
-        self.relu1 = nn.ReLU()
-        self.maxpool1 = nn.MaxPool2d(2, stride=2)
+        self.latent_dim = latent_dim
         
-        self.conv2 = nn.Conv2d(16, 32, 3, stride=1, padding="same")
-        self.relu2 = nn.ReLU()
-        self.maxpool2 = nn.MaxPool2d(2, stride=2)
+        # encoder block
+        self.encoder = nn.Sequential(
+            # conv blocks
+            nn.Conv2d(3, 16, 3, stride=1, padding=1),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(2, stride=2),
+            
+            nn.Conv2d(16, 32, 3, stride=1, padding=1),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(2, stride=2),
+            
+            nn.Conv2d(32, 64, 3, stride=1, padding=1),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(2, stride=2),
+            
+            # project data to latent space
+            nn.Flatten(),
+            nn.Linear(64*4*4, self.latent_dim)
+        )
         
-        self.conv3 = nn.Conv2d(32, 64, 3, stride=1, padding="same")
-        self.relu3 = nn.ReLU()
-        self.maxpool3 = nn.MaxPool2d(2, stride=2)
-        
-        # define decoder blocks
-        self.t_conv1 = nn.ConvTranspose2d(64, 32, 2, stride=2)
-        self.t_relu1 = nn.ReLU()
-        self.t_conv2 = nn.ConvTranspose2d(32, 8, 2, stride=2)
-        self.t_relu2 = nn.ReLU()
-        self.t_conv3 = nn.ConvTranspose2d(8, 3, 2, stride=2)
-        self.t_sigmoid = nn.Sigmoid()
+        self.decoder = nn.Sequential(
+            # transform latent space data back to 3 channel
+            nn.Linear(self.latent_dim, 64*4*4),
+            Reshape(-1, 64, 4, 4),
+            
+            # transpose conv blocks
+            nn.ConvTranspose2d(64, 32, 2, stride=2),
+            nn.LeakyReLU(),
+            
+            nn.ConvTranspose2d(32, 8, 2, stride=2),
+            nn.LeakyReLU(),
+            
+            nn.ConvTranspose2d(8, 3, 2, stride=2),
+            nn.Sigmoid()
+        )
+
     
     def forward(self, x):
         # encoder block
-        out = self.conv1(x)
-        out = self.relu1(out)
-        out = self.maxpool1(out)
-        
-        out = self.conv2(out)
-        out = self.relu2(out)
-        out = self.maxpool2(out)
-        
-        out = self.conv3(out)
-        out = self.relu3(out)
-        out = self.maxpool3(out)
-        
-        # decoder block
-        out = self.t_conv1(out)
-        out = self.t_relu1(out)
-        
-        out = self.t_conv2(out)
-        out = self.t_relu2(out)
-        
-        out = self.t_conv3(out)
-        out = self.t_sigmoid(out)
-        
+        out = self.encoder(x)
+        out = self.decoder(out)
         return out
     
     
